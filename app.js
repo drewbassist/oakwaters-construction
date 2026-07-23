e852675fea7e60d4c7bd810a1863164d5f40f168
drewbassist <drewbass@gmail.com>
2026-07-23 15:13:05 -0700

    Delete app.js

--- app.js
+++
@@ -1,1 +0,0 @@
-const toggle=document.querySelector('.mobile-toggle');const links=document.querySelector('.nav-links');if(toggle&&links){toggle.addEventListener('click',()=>links.classList.toggle('open'));}
